a752486200f79a145e568b6f8f5b32ea19191bd3
Leelsmuth <femianjorin@rocketmail.com>
2019-08-21 17:16:43 +0100

    completed the prototype.js portion of the project

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -46,7 +46,23 @@
   - Give persons the ability to poop.
   - When pooping, the stomach should empty.
 */
+function Person(name, age) {
+  this.name = name;
+  this.age = Number(age);
+  this.greet = `${this.name} ${this.age}`;
+  this.eatEdible = 'I eat I eat I eat...Muah HaHa';
+  this.stomach = [];
+  this.poop = this.stomach;
+}
+Person.prototype.eating = function () {
+  return this.stomach.push = this.eatEdible;
+}
 
+Person.prototype.pooping = function () {
+  return this.stomach;
+}
+
+var femi = new Person('Oluwafemi', '29');
 /*
   TASK 2
 
@@ -58,7 +74,32 @@
   - Give cars the ability to be repaired.
   - A repaired car can be driven again.
 */
-
+function Car (modelName, carMake) {
+  this.model = modelName;
+  this.make = carMake;
+  this.drive = true;
+  this.crash = true;
+  this.repair = true;
+  this.odometer = 0;
+}
+Car.prototype.driveDistance = function () {
+  return this.odometer += 50;
+}
+Car.prototype.carCrashed = function () {
+  if (this.crash === true)  {
+    console.log(`I crashed at x miles!`);
+    return this.drive = false
+  }
+  return this.drive = true;
+}
+Car.prototype.carRepaired = function () {
+  if (this.repair === true) {
+    console.log(`You have fixed your ${this.model}. Take it for a spin!!!`)
+    return this.drive;
+  }
+  return this.drive = false;
+}
+var vehicle = new Car('Toyota', 'Sedan');
 /*
   TASK 3
 
@@ -67,6 +108,17 @@
   - Babies should have the ability to play, which persons don't.
   - By playing, a string is returned with some text of your choosing.
 */
+function Baby (name, age,) {
+  Person.call(this, name, age);
+  this.play = `a-ga, a-ba, a-da`;
+}
+
+Baby.prototype = Object.create(Person.prototype);
+var baby = new Baby('Jamie', 1, 'trains'); // just copy
+Baby.prototype.playBaby = function () {
+  return `Baby likes to play.. Baby likes to play`;
+}
+
 
 /*
   TASK 4
@@ -76,7 +128,26 @@
   complicated one with lots of state. Surprise us!
 
 */
+function Life(name, placeOfBirth, death) {
+  this.name = name;
+  this.place = placeOfBirth;
+  this.death = Number(death);
+  this.college = true;
+  this.goodLife = `${this.name}, I guess you lived a good Life.`;
+  this.justOK = `${this.name}, life did nt turn out how you planned it. At least you still enjoyed your old age. I guess you can't complain too much`
+  this.notJustOk = `${this.name}, Hmmmmmmmmmmmmmmmm`
+}
+Life.prototype.checkLife = function () {
+  if (this.death < 50) {
+    return this.notJustOk;
+  } else if (this.death < 70 ) {
+    return this.justOK;
+  } else {
+    return this.goodLife;
+  }  
+}
 
+var james = new Life('James', 'Nigeria', '80')
 /*
 
   STRETCH TASK
--- assignments/this.js
+++ assignments/this.js
@@ -12,15 +12,23 @@
 // Principle 1
 
 // code example for Window Binding
-
+var myName = 'Oluwafemi';
 // Principle 2
 
 // code example for Implicit Binding
-
+function myName () {
+    return this;
+}
 // Principle 3
 
 // code example for New Binding
-
+ function MyName (name) {
+     this.name = name;
+ }
 // Principle 4
 
 // code example for Explicit Binding
+function myRealName (name, address) {
+    return `${name}, your address is ${address}`
+}
+var me = myRealName.bind(null,'Oluwafemi', 'Gbagada, Lagos' );
