2275f92a400b2e70fba14548b6ec52d09db6958a
Leelsmuth <femianjorin@rocketmail.com>
2019-08-21 12:29:12 +0100

    initial commit

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -45,7 +45,9 @@
   - When eating an edible, it should be pushed into a "stomach" property which is an array.
   - Give persons the ability to poop.
   - When pooping, the stomach should empty.
+*/
 
+/*
   TASK 2
 
   - Build a Car constructor that takes model name and make.
@@ -55,14 +57,18 @@
   - A crashed car can't be driven any more. Attempts return a string "I crashed at x miles!", x being the miles in the odometer.
   - Give cars the ability to be repaired.
   - A repaired car can be driven again.
+*/
 
+/*
   TASK 3
 
   - Build a Baby constructor that subclasses the Person built earlier.
   - Babies of course inherit the ability to greet, which can be strange.
   - Babies should have the ability to play, which persons don't.
   - By playing, a string is returned with some text of your choosing.
+*/
 
+/*
   TASK 4
 
   Use your imagination and come up with constructors that allow to build objects
